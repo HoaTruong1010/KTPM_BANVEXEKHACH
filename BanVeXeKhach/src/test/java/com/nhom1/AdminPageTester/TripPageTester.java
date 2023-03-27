f2b58a9c9a80a970387ab83aee5017c0f0783b62
Haunguyen42193 <haunguyen42193@gmail.com>
2023-03-27 22:43:19 +0700

    TEST1

--- BanVeXeKhach/src/test/java/com/nhom1/AdminPageTester/TripPageTester.java
+++ BanVeXeKhach/src/test/java/com/nhom1/AdminPageTester/TripPageTester.java
@@ -6,9 +6,7 @@ package com.nhom1.AdminPageTester;
 
 import com.nhom1.services.JDBCUtils;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -24,7 +22,11 @@ public class TripPageTester {
     public static void BeforeALL() throws SQLException {
         conn = JDBCUtils.createConn();
     }
-    
+    @Test
+    public void testSearch() {
+        
+        Assertions.assertTrue(0!=1);
+    }
     @AfterAll
     public static void AfterAll() throws SQLException {
         if (conn != null) {
